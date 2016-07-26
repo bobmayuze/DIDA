11be9ca2c762ef982d9aef20a50dfaf60d136459
bobmayuze <584653629@qq.com>
2016-07-26 15:35:42 +0800

    Update
    Update

---
+++ DIDA_Squad_D/Input/2.Light/Photo Resistor/Photo_Resistor.ino
@@ -0,0 +1,16 @@
+int led = 9;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+
+void loop(){
+  int sensorValue = analogRead(A0);
+
+  Serial.println(sensorValue);
+  analogWrite(led,sensorValue/4);
+
+  delay(1000);
+}
