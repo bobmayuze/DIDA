fb0680c29600b2bf7051ee35db32f15bb11dc2d1
bobmayuze <584653629@qq.com>
2016-07-30 21:27:55 +0800

    Update
    Update

---
+++ DIDA_Squared_D/2.Output/1.Light/1.LED/2.Double/Double.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // initialize digital pin 13 and pin 12 as an output.
+  pinMode(13, OUTPUT); //
+  pinMode(12, OUTPUT); //
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);  
+}
